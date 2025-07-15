e07931be8f4a58cd73b013cb4f5301e576d0640c
kimtaehyeong99 <kth@robotis.com>
2025-07-15 09:48:07 +0900

    Created FFW_BG2_PICK_PLACE_CFG version for FFW_BG2 robot.
    Signed-off-by: kimtaehyeong99 <kth@robotis.com>

--- source/robotis_lab/robotis_lab/assets/FFW_BG2.py
+++ source/robotis_lab/robotis_lab/assets/FFW_BG2.py
@@ -24,7 +24,7 @@ FFW_BG2_CFG = ArticulationCfg(
     spawn=UsdFileCfg(
         usd_path=f"{ROBOTIS_LAB_ASSETS_DATA_DIR}/robots/FFW/FFW_BG2.usd",
         rigid_props=RigidBodyPropertiesCfg(
-            disable_gravity=False,
+            disable_gravity=True,
             max_depenetration_velocity=5.0,
         ),
         articulation_props=ArticulationRootPropertiesCfg(
@@ -70,8 +70,8 @@ FFW_BG2_CFG = ArticulationCfg(
             ],
             velocity_limit_sim=10.0,
             effort_limit_sim=1000.0,
-            stiffness=400.0,
-            damping=80.0,
+            stiffness=100.0,
+            damping=20.0,
         ),
 
         # Actuators for grippers
@@ -81,18 +81,48 @@ FFW_BG2_CFG = ArticulationCfg(
                 "gripper_r_joint[1-4]",
             ],
             velocity_limit_sim=5.0,
-            effort_limit_sim=5000.0,
-            stiffness=100000.0,
-            damping=100.0,
+            effort_limit_sim=100.0,
+            stiffness=80.0,
+            damping=10.0,
         ),
 
         # Actuators for head joints
         "head": ImplicitActuatorCfg(
             joint_names_expr=["head_joint1", "head_joint2"],
             velocity_limit_sim=5.0,
-            effort_limit_sim=300.0,
-            stiffness=200.0,
-            damping=50.0,
+            effort_limit_sim=30.0,
+            stiffness=30.0,
+            damping=10.0,
         ),
     }
+)
+
+FFW_BG2_PICK_PLACE_CFG = FFW_BG2_CFG.replace(
+    spawn=FFW_BG2_CFG.spawn,
+    actuators=FFW_BG2_CFG.actuators,
+    init_state=ArticulationCfg.InitialStateCfg(
+        joint_pos={
+            # Lift joint
+            "lift_joint": 0.0,
+            
+            # Left arm joints
+            **{f"arm_l_joint{i+1}": 0.0 for i in range(7)},
+            # Right arm joints
+            "arm_r_joint1": 0.0,
+            "arm_r_joint2": -1.13,
+            "arm_r_joint3": 0.03,
+            "arm_r_joint4": -2.1,
+            "arm_r_joint5": -1.44,
+            "arm_r_joint6": 0.43,
+            "arm_r_joint7": -0.65,
+
+            # Left and right gripper joints
+            **{f"gripper_l_joint{i+1}": 0.0 for i in range(4)},
+            **{f"gripper_r_joint{i+1}": 0.0 for i in range(4)},
+
+            # Head joints
+            "head_joint1": 0.60,
+            "head_joint2": -0.17,
+        }
+    ),
 )
