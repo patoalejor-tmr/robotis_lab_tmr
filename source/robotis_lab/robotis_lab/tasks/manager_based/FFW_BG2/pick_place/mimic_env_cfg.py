e2b94fde235714d6d7b2348aee18432a709f85d2
kimtaehyeong99 <kth@robotis.com>
2025-07-16 12:17:28 +0900

    Add the end-effector's position and orientation to the observation space for the pick and place task.
    Signed-off-by: kimtaehyeong99 <kth@robotis.com>

--- source/robotis_lab/robotis_lab/tasks/manager_based/FFW_BG2/pick_place/mimic_env_cfg.py
+++ source/robotis_lab/robotis_lab/tasks/manager_based/FFW_BG2/pick_place/mimic_env_cfg.py
@@ -17,7 +17,7 @@ from .ik_rel_env_cfg import PickPlaceFFWBG2EnvCfg
 @configclass
 class PickPlaceFFWBG2MimicEnvCfg(PickPlaceFFWBG2EnvCfg, MimicEnvCfg):
     """
-    Isaac Lab Mimic environment config class for Franka Cube Stack IK Rel Visuomotor env.
+    Isaac Lab Mimic environment config class for FFW BG2 Pick and Place Visuomotor env.
     """
 
     def __post_init__(self):
@@ -27,14 +27,66 @@ class PickPlaceFFWBG2MimicEnvCfg(PickPlaceFFWBG2EnvCfg, MimicEnvCfg):
         # Override the existing values
         self.datagen_config.name = "demo_src_pick_place_isaac_lab_task_D0"
         self.datagen_config.generation_guarantee = True
-        self.datagen_config.generation_keep_failed = True
-        self.datagen_config.generation_num_trials = 10
+        self.datagen_config.generation_keep_failed = False
+        self.datagen_config.generation_num_trials = 1000
         self.datagen_config.generation_select_src_per_subtask = False
+        self.datagen_config.generation_select_src_per_arm = False
+        self.datagen_config.generation_relative = False
+        self.datagen_config.generation_joint_pos = False
         self.datagen_config.generation_transform_first_robot_pose = False
         self.datagen_config.generation_interpolate_from_last_target_pose = True
-        self.datagen_config.generation_relative = True
         self.datagen_config.max_num_failures = 25
+        self.datagen_config.num_demo_to_render = 10
+        self.datagen_config.num_fail_demo_to_render = 25
         self.datagen_config.seed = 1
 
         # The following are the subtask configurations for the stack task.
         subtask_configs = []
+        subtask_configs.append(
+            SubTaskConfig(
+                # Each subtask involves manipulation with respect to a single object frame.
+                object_ref="object",
+                # This key corresponds to the binary indicator in "datagen_info" that signals
+                # when this subtask is finished (e.g., on a 0 to 1 edge).
+                subtask_term_signal="idle_right",
+                first_subtask_start_offset_range=(0, 0),
+                # Randomization range for starting index of the first subtask
+                subtask_term_offset_range=(0, 0),
+                # Selection strategy for the source subtask segment during data generation
+                # selection_strategy="nearest_neighbor_object",
+                selection_strategy="nearest_neighbor_object",
+                # Optional parameters for the selection strategy function
+                selection_strategy_kwargs={"nn_k": 3},
+                # Amount of action noise to apply during this subtask
+                action_noise=0.005,
+                # Number of interpolation steps to bridge to this subtask segment
+                num_interpolation_steps=0,
+                # Additional fixed steps for the robot to reach the necessary pose
+                num_fixed_steps=0,
+                # If True, apply action noise during the interpolation phase and execution
+                apply_noise_during_interpolation=False,
+            )
+        )
+        subtask_configs.append(
+            SubTaskConfig(
+                # Each subtask involves manipulation with respect to a single object frame.
+                object_ref="object",
+                # Corresponding key for the binary indicator in "datagen_info" for completion
+                subtask_term_signal=None,
+                # Time offsets for data generation when splitting a trajectory
+                subtask_term_offset_range=(0, 0),
+                # Selection strategy for source subtask segment
+                selection_strategy="nearest_neighbor_object",
+                # Optional parameters for the selection strategy function
+                selection_strategy_kwargs={"nn_k": 3},
+                # Amount of action noise to apply during this subtask
+                action_noise=0.005,
+                # Number of interpolation steps to bridge to this subtask segment
+                num_interpolation_steps=3,
+                # Additional fixed steps for the robot to reach the necessary pose
+                num_fixed_steps=0,
+                # If True, apply action noise during the interpolation phase and execution
+                apply_noise_during_interpolation=False,
+            )
+        )
+        self.subtask_configs["ffw_bg2"] = subtask_configs
